de61857cf9e5d5731d9a75403ef03385e0f34480
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-12-22 02:08:21 +0530

    initial jest setup

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,1 +1,21 @@
-// Start your implementation here
+function bizarreStringIncrementer(str) {
+    if(isNaN(str.slice(-1))) {
+      return `${str}1`;
+    }
+    else {
+      var alphabets = str.split('') .filter(n => isNaN(n)).join('');
+      var digits = str.split('') .filter(n => !isNaN(n)).join('');
+      var newNum = +digits + 1;
+      if(digits.length > newNum.toString().length) {
+        var bizarreString  = '';
+        for(var i = 0; i < digits.length; i++) {
+          bizarreString  += '0';
+        }
+        return alphabets + (bizarreString  + newNum).slice(-digits.length);
+      }
+      else {
+        return alphabets + newNum;
+      }
+    }
+  }
+  module.exports = { bizarreStringIncrementer };
